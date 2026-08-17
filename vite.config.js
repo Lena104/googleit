e3b726eb57d7b7d63da366fb179fe37428d67e0e
Lena <109345641+Lena104@users.noreply.github.com>
2026-08-17 20:09:07 +0900

    Restore GoogleIt GitHub Pages (#1)
    Co-authored-by: kelonInstitute <kelon@esay.co.kr>

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 맞춤 도메인 사용시 기본 경로를 루트로 설정
+  base: '/googleit/',
 })
